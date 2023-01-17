63659ecd190559edf3925c6ae2cc8a87187b4ce8
Yuri Ferreira <yuri.ferreira@superlogica.com>
2023-01-16 23:37:17 -0300

    criando playlist e padronizando rota

--- app/Http/Controllers/PlaylistController.php
+++ app/Http/Controllers/PlaylistController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Musica;
 use App\Models\Playlist;
 use App\Services\MusicaService;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class PlaylistController extends Controller
 {
@@ -16,7 +18,55 @@ class PlaylistController extends Controller
                 ->toArray();
             return response()->json($playlist, 200);
         } catch (\Throwable $th) {
-            return response()->json('Erro ao listar', 500);
+            return response()->json($th->getMessage(), 500);
+        }
+    }
+
+    public function nova(Request $request)
+    {
+        try {
+            $playlist = Playlist::create([
+                'titulo' => $request->titulo,
+                'dia' => Carbon::createFromFormat('d/m/Y', $request->dia)->format('Y-m-d')
+            ]);
+            return response()->json($playlist, 200);
+        } catch (\Throwable $th) {
+            return response()->json($th->getMessage(), 500);
+        }
+    }
+
+    public function addMusica(Request $request)
+    {
+        try {
+            $playlist = Playlist::find($request->playlist_id);
+            $playlist->musicas()->attach($request->musica_id);
+            return response()->json('Música adicionada', 200);
+        } catch (\Throwable $th) {
+            return response()->json($th->getMessage(), 500);
+        }
+    }
+
+    public function removerMusica(Request $request)
+    {
+        try {
+            $playlist = Playlist::find($request->playlist_id);
+            $playlist->musicas()->detach($request->musica_id);
+            return response()->json('Música removida', 200);
+        } catch (\Throwable $th) {
+            return response()->json($th->getMessage(), 500);
+        }
+    }
+
+
+    public function remover(Request $request)
+    {
+        try {
+            $playlist = Playlist::find($request->playlist_id);
+            $playlist->musicas()->sync([]);
+            $playlist->delete();
+            return response()->json('Playlist Removida', 200);
+        } catch (\Throwable $th) {
+            return response()->json($th->getMessage(), 500);
         }
     }
 
--- routes/web.php
+++ routes/web.php
@@ -4,27 +4,35 @@
 
 use App\Models\Parte;
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+
+    $router->get('/version', function () use ($router) {
+        return $router->app->version();
+    });
+    $router->get('/partes', function () {
+        return Parte::all()->toArray();
+    });
+
+    $router->get('/buscar/{termo}', ['uses' => 'MusicaController@buscar']);
+    $router->get('/baixar/{id}', ['uses' => 'MusicaController@baixar']);
+
+    $router->get('/playlist', ['uses' => 'PlaylistController@listar']);
+    $router->get('/playlist/{id}', ['uses' => 'PlaylistController@musicas']);
+    $router->post('/playlist/nova', ['uses' => 'PlaylistController@nova']);
+    $router->post('/playlist/add-musica', ['uses' => 'PlaylistController@addMusica']);
+    $router->post('/playlist/remover-musica', ['uses' => 'PlaylistController@removerMusica']);
+    $router->post('/playlist/remover', ['uses' => 'PlaylistController@remover']);
+
+    $router->get('/cifra/{id}', ['uses' => 'CifraController@visualizar']);
+
+    $router->get('/musicas', ['uses' => 'MusicaController@listar']);
+    $router->get('/musicas/{id}', ['uses' => 'MusicaController@visualizar']);
+    $router->post('/musicas/remover-secao', ['uses' => 'MusicaController@removerSecao']);
+    $router->post('/musicas/nova-secao', ['uses' => 'MusicaController@novaSecao']);
+    $router->post('/musicas/atualizar-secao', ['uses' => 'MusicaController@atualizarSecao']);
+    $router->post('/musicas/atualizar-ordem', ['uses' => 'MusicaController@atualizarOrdem']);
+    $router->post('/musicas/nova', ['uses' => 'MusicaController@nova']);
+
+
 });
-
-$router->get('/partes', function () {
-    return Parte::all()->toArray();
-});
-
-$router->get('/buscar/{termo}', ['uses' => 'MusicaController@buscar']);
-$router->get('/baixar/{id}', ['uses' => 'MusicaController@baixar']);
-
-$router->get('/playlist', ['uses' => 'PlaylistController@listar']);
-$router->get('/playlist/{id}', ['uses' => 'PlaylistController@musicas']);
-
-$router->get('/cifra/{id}', ['uses' => 'CifraController@visualizar']);
-
-$router->get('/musicas', ['uses' => 'MusicaController@listar']);
-$router->get('/musicas/{id}', ['uses' => 'MusicaController@visualizar']);
-$router->post('/musicas/remover-secao', ['uses' => 'MusicaController@removerSecao']);
-$router->post('/musicas/nova-secao', ['uses' => 'MusicaController@novaSecao']);
-$router->post('/musicas/atualizar-secao', ['uses' => 'MusicaController@atualizarSecao']);
-$router->post('/musicas/atualizar-ordem', ['uses' => 'MusicaController@atualizarOrdem']);
-$router->post('/musicas/nova', ['uses' => 'MusicaController@nova']);
-
